8e1fe88a497ea80b2d9a06829c002307a91a884a
aniran mohammadpour <62486805+aniranmp@users.noreply.github.com>
2020-07-27 12:23:13 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LongestPalindromSubString
+{ 
+
+class Program
+{
+
+	public static void printSubStr(string str,int low, int high)
+	{
+		Console.WriteLine(str.Substring(low,(high + 1) - low));
+	}
+	public static int longestPalSubstr(string str)
+	{
+		int maxLength = 1; // The result length
+
+		int start = 0;
+		int len = str.Length;
+
+		int low, high;
+
+		for (int i = 1; i < len; ++i)
+		{
+			low = i - 1;
+			high = i;
+			while (low >= 0 && high < len && str[low] == str[high])
+			{
+				if (high - low + 1 > maxLength)
+				{
+					start = low;
+					maxLength = high - low + 1;
+				}
+				--low;
+				++high;
+			}
+
+			low = i - 1;
+			high = i + 1;
+			while (low >= 0 && high < len && str[low] == str[high])
+			{
+				if (high - low + 1 > maxLength)
+				{
+					start = low;
+					maxLength = high - low + 1;
+				}
+				--low;
+				++high;
+			}
+		}
+
+		Console.Write("Longest palindrome substring is: ");
+		printSubStr(str, start, start + maxLength - 1);
+
+		return maxLength;
+	}
+
+	// Driver Code 
+	public static void Main(string[] args)
+	{
+			Console.WriteLine("Enter Your String : ");
+			bool flag = false;
+		string str = Console.ReadLine();
+			while(!flag)
+			if (string.IsNullOrWhiteSpace(str){
+				flag = true;
+				Console.WriteLine("Length is: " + longestPalSubstr(str));
+
+			}
+			else
+			{
+				flag = false;
+				Console.WriteLine("Please Write")
+			}
+	}
+}
+}
